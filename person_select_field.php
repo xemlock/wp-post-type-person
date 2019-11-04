b0e4447c1e7eb060746513453ff68e5e92984740
xemlock <xemlock@gmail.com>
2019-11-04 22:32:18 +0100

    Person select WIP

--- person_select_field.php
+++ person_select_field.php
@@ -53,44 +53,66 @@ function person_select_field(array $field)
     $id = isset($field['id']) ? $field['id'] : ('person-select-' . mt_rand(1E5, 1E6 - 1));
 
     $multiple = !empty($field['multiple']);
-    $sortable = !empty($field['sortable']);
+    $sortable = $multiple && !empty($field['sortable']);
 
-    $name = $multiple ? $field['name'] : ($field['name'] . '[]');
-    $value = isset($field['value']) ? array_map('intval', (array) $field['value']) : array();
+    $name = $field['name'] . ($multiple ? '[]' : '');
+    $value = array();
 
-    // fetch people from value
+    if (isset($field['value'])) {
+        $value = array_filter(array_map('intval', (array) $field['value']), 'intval');
+    }
+
     /** @var WP_Post[] $people */
     if (count($value)) {
         $people = get_posts(array(
-            'post_type'      => array('person'),
-            'post_status'    => 'publish',
-            'nopaging'       => true,
-            'post__in'       => $value,
-            'posts_per_page' => 100,
+            'post_type'   => array('person'),
+            'post_status' => 'publish',
+            'nopaging'    => true,
+            'post__in'    => $value,
         ));
     } else {
         $people = array();
     }
 ?>
-<div>
-    <div>
+<div class="person-select" id="<?php esc_attr_e($id) ?>">
+    <div class="person-select__value-inputs">
+        <?php foreach ($value as $val): ?>
+        <input type="hidden" name="<?php esc_attr_e($name) ?>" value="<?php esc_attr_e($val) ?>" />
+        <?php endforeach ?>
+    </div>
+    <div class="person-select__selected-items">
         <?php foreach ($people as $person): ?>
             <?php echo get_post_meta($person->ID, 'person__first_name', true) ?>
             <?php echo get_post_meta($person->ID, 'person__last_name', true) ?>
             <?php echo ____wp_get_post_thumbnail_url($person->ID) ?>
         <?php endforeach ?>
     </div>
-    <div>
-        <?php foreach ($value as $val): ?>
-        <input type="hidden" name="<?php esc_attr_e($name) ?>" value="<?php esc_attr_e($val) ?>" />
-        <?php endforeach ?>
+    <div class="person-select__search">
+        <input type="text" class="person-select__search-input" />
+        <button class="person-select__search-button button button-large button-primary" type="button" disabled><?php esc_html_e('Add') ?></button>
     </div>
-    <input type="text" id="<?php esc_attr_e($id) ?>" />
-    <button class="button button-large button-primary" type="button" disabled><?php esc_html_e('Add') ?></button>
 </div>
 <script>
 jQuery(function ($) {
-    var autocomplete = $('#<?php esc_attr_e($id) ?>').autocomplete({
+    var closeTime;
+    var selected = [];
+    var field = $('.person-select#<?php esc_attr_e($id) ?>');
+    var sortable = <?php echo json_encode($sortable) ?>;
+    var multiple = <?php echo json_encode($multiple) ?>;
+
+    var searchInput = field.find('.person-select__search-input');
+    var searchButton = field.find('.person-select__search-button');
+
+    if (sortable) {
+        field.addClass('is-sortable');
+        field.find('.person-select__selected-items').sortable({items: '.xxx'});
+    }
+
+    if (multiple) {
+        field.addClass('is-multiple');
+    }
+
+    var autocomplete = searchInput.autocomplete({
         source: function (request, response) {
             $.ajax({
                 url: '<?php echo admin_url('admin-ajax.php') ?>',
@@ -104,9 +126,62 @@ jQuery(function ($) {
                 }
             });
         },
+        open: function (event, ui) {
+            closeTime = null;
+        },
+        close: function (event, ui) {
+            closeTime = event.timeStamp;
+        },
+        focus: function (event, ui) {
+            event.preventDefault();
+            searchInput.val(ui.item.full_name);
+        },
+        select: function(event, ui) {
+            event.preventDefault();
+
+            if (!multiple && selected.length) {
+                return;
+            }
+
+            for (var i = 0; i < selected.length; ++i) {
+                if (selected[i] === ui.item.ID) {
+                    searchInput.val('');
+                    searchButton.prop('disabled', true);
+                    return;
+                }
+            }
+            selected.push(ui.item.ID);
+
+            if (!multiple) {
+                field.find('.person-select__value-inputs').empty();
+            }
+
+            var valueInput = $('<input type="hidden" />').attr('name', '<?php echo esc_js($name) ?>').val(ui.item.ID);
+            field.find('.person-select__value-inputs').append(valueInput);
+            field.find('.person-select__selected-items').append(
+                $('<div class="xxx" />').attr('data-id', ui.item.ID)
+                    .append($('<img/>').attr('src', ui.item.thumbnail_url))
+                    .append($('<div/>').text(ui.item.full_name))
+                    .append($('<button type="button" />').text('DELETE').on('click', function (e) {
+                        e.preventDefault();
+                        selected.splice(selected.indexOf(ui.item.ID), 1);
+                        valueInput.remove();
+                        valueInput = null;
+                        $(this).closest('.xxx').remove();
+                    }))
+            );
+            searchInput.val('');
+            searchButton.prop('disabled', true);
+
+            if (!multiple) {
+                field.find('person-select__search').hide();
+            }
+            return false;
+        },
     }).autocomplete('instance');
 
-    $('#<?php esc_attr_e($id) ?>').on('keydown keyup keypress', function (e) {
+    searchInput.attr('autocomplete', Math.random().toFixed(16));
+    searchInput.on('keydown keyup keypress', function (e) {
         console.log(e.type, e.key, e.which);
         if (e.which === 13) {
             e.preventDefault();
@@ -114,9 +189,13 @@ jQuery(function ($) {
         }
     });
 
+    searchInput.on('click', function (e) {
+
+    });
+
     autocomplete._renderItem = function (ul, item) {
         return $('<li>')
-            .append('<div>' + item.full_name + '<br><img src="' + item.thumbnail_url + '" /></div>')
+            .append('<div>' + item.full_name + '<br><img src="' + (item.thumbnail_url || '') + '" alt="" /></div>')
             .appendTo(ul);
     };
 
